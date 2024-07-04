083abf0b6d7d23b580fdc9077c028d3a157a7790
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-04 19:36:24 +0100

    Add functionality to add new books to the library

--- bookLibrary.js
+++ bookLibrary.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 const libraryBooks= document.querySelector(".library-books");
+const addBookForm = document.querySelector("#add-book-form");
 
 // Book constructor
 function Book(title, author, pages, hasRead) {
@@ -21,22 +22,69 @@ function addBookToLibrary ({title, author, pages, hasRead} = {}) {
     myLibrary.push(newBook);
 }
 
-// preload the library with some books
-addBookToLibrary({title:"The Hobbit", author:"J.R.R Tolkien", pages:"295", hasRead:false})
-addBookToLibrary({title:"Gentle and Lowly", author:"Dane Ortlund", pages:"198", hasRead:true})
-addBookToLibrary({title:"On Death", author:"Tim Keller", pages:"80", hasRead:true})
-addBookToLibrary({title:"The Four Loves", author:"CS Lewis", pages:"100", hasRead:false})
-addBookToLibrary({title:"You are what you love", author:"James K.A. Smith", pages:"150", hasRead:false})
+function preloadLibrary() {
+    addBookToLibrary({title:"The Hobbit", author:"J.R.R Tolkien", pages:"295", hasRead:false})
+    addBookToLibrary({title:"Gentle and Lowly", author:"Dane Ortlund", pages:"198", hasRead:true})
+    addBookToLibrary({title:"On Death", author:"Tim Keller", pages:"80", hasRead:true})
+    addBookToLibrary({title:"The Four Loves", author:"CS Lewis", pages:"100", hasRead:false})
+    addBookToLibrary({title:"You are what you love", author:"James K.A. Smith", pages:"150", hasRead:false})
+}
 
-// display books 
-myLibrary.forEach((book) => {
-    const tableRow = document.createElement("tr");
-    for (const key in book) {
-        if (typeof book[key] !== "function") {
-            const tableData = document.createElement("td");
-            tableData.textContent = book[key];
-            tableRow.appendChild(tableData);
+function displayBooks(myLibrary, index=0) {
+    let i = index;
+    while (i < myLibrary.length) {
+        const tableRow = document.createElement("tr");
+        const book = myLibrary[i];
+        for (const key in book) {
+            if (typeof book[key] !== "function") {
+                const tableData = document.createElement("td");
+                tableData.textContent = book[key];
+                tableRow.appendChild(tableData);
+            }
+        }
+        libraryBooks.appendChild(tableRow);
+
+        i++;
+    }
+
+}
+
+// preload the library with some books
+preloadLibrary();
+
+// display the books
+displayBooks(myLibrary);
+
+// Capture form input
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // capture all inputs of the form
+    let bookTitle = document.querySelector("#book-title");
+    let bookAuthor = document.querySelector("#book-author");
+    let bookPages = document.querySelector("#book-pages");
+    let bookReadOptions = document.querySelectorAll("input[name='book-read']")
+    let bookRead;
+    for (let i = 0; i < bookReadOptions.length; i++) {
+        if (bookReadOptions[i].checked) {
+            bookRead = bookReadOptions[i]
         }
     }
-    libraryBooks.appendChild(tableRow);
+    
+    // Add new book to library
+    addBookToLibrary({
+        title: bookTitle.value, 
+        author: bookAuthor.value, 
+        pages: bookPages.value, 
+        hasRead: bookRead.value === "yes" ? true : false
+    });
+    
+    // Append the new book to the table
+    displayBooks(myLibrary, myLibrary.length-1);
+
+    // Clear form fields
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    document.querySelector("#book-read-yes").checked = false;
+    document.querySelector("#book-read-no").checked = true;
 });
